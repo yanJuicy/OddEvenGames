6e8918b4d328b19ef43f09e9fdc379ef356e2397
yanJuicy <hanjub3@gmail.com>
2023-06-14 17:32:19 +0900

    DataType

---
+++ playground/DataType.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+var isChecked: Bool = true
+
+var tempearture: Int = -100
+
+var cakes: UInt = 100
+
+var pi: Float = 3.14
+
+var pi2: Double = 3.14
+
+var sampleCharacter: Character = "A"
+
+var sampleString: String = "Hi"
+
+var test = 100
+type(of: test)
+
+var sampleAny: Any = "Any"
+sampleAny = 10000
+sampleAny = 3.14
+
+var sampleInt: Int? = nil
+type(of: sampleInt)
+
+var nilString: String? = nil
+type(of: nilString)
+
+
